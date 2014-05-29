0fb2848aa148e66a1e9c154788e7524df9b90cc4
phenomenz <david@reallysuccessful.com>
2014-05-29 09:47:33 -0400

    Project import

---
+++ ant-gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/ant-gen/io/popcorntime/frontend/R.java \
+ : /Users/david/dev/popcorn-mobile/frontend/platforms/android/res/drawable/icon.png \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/drawable-hdpi/icon.png \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/drawable-ldpi/icon.png \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/drawable-mdpi/icon.png \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/values/strings.xml \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/res/xml/config.xml \
+/Users/david/dev/popcorn-mobile/frontend/platforms/android/ant-build/AndroidManifest.xml \
