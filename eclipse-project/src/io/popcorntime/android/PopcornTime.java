d08dc787a467574d0bbce1f131b53dc9bb5f9d9f
Jimmy Thompson <jimmy@jimmythompson.co.uk>
2014-06-03 23:59:11 +0100

    Re-organised PopcornTime class

--- eclipse-project/src/io/popcorntime/android/PopcornTime.java
+++ eclipse-project/src/io/popcorntime/android/PopcornTime.java
@@ -4,6 +4,7 @@ import org.apache.cordova.Config;
 import org.apache.cordova.CordovaActivity;
 import org.nodejs.core.NodeJSService;
 import org.nodejs.core.NodeJSService.LocalBinder;
+
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -12,38 +13,36 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class PopcornTime extends CordovaActivity 
+public class PopcornTime extends CordovaActivity
 {
-   boolean mBounded;
-   NodeJSService mServer;
+    boolean mBound;
+    NodeJSService mServer;
+
+    ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Toast.makeText(PopcornTime.this, "Service is connected", Toast.LENGTH_LONG).show();
+
+            mBound = true;
+            mServer = ((LocalBinder) service).getActivity();
+       }
+
+       public void onServiceDisconnected(ComponentName name) {
+            Toast.makeText(PopcornTime.this, "Service is disconnected", Toast.LENGTH_LONG).show();
+
+            mBound = false;
+            mServer = null;
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-      super.onCreate(savedInstanceState);
-    
+        super.onCreate(savedInstanceState);
         super.init();
-        Intent mIntent = new Intent(this, NodeJSService.class);
-        startService(mIntent);
-  
-        Log.d("Popcormtime", "App created!");
-        // Set by <content src="index.html" /> in config.xml
+
+        startService(new Intent(this, NodeJSService.class));
+
+        Log.d("PopcornTime", "App created!");
         super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html");
-    } 
-    ServiceConnection mConnection = new ServiceConnection() {
-        
-        public void onServiceDisconnected(ComponentName name) {
-         Toast.makeText(PopcornTime.this, "Service is disconnected", 1000).show();
-         mBounded = false;
-         mServer = null;
-        }
-        
-        public void onServiceConnected(ComponentName name, IBinder service) {
-         Toast.makeText(PopcornTime.this, "Service is connected", 1000).show();
-         mBounded = true;
-         LocalBinder mLocalBinder = (LocalBinder)service;
-         mServer = mLocalBinder.getActivity();
-        }
-       };
+    }
 }
